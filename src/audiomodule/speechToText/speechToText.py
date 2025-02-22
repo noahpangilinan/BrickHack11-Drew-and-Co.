a12936234ffe7b6b3a93f9264c2e5446bb41cc64
Noah Pangilinan <noahpangilinan1@gmail.com>
2025-02-22 18:00:17 -0500

    Bigger buffer and file writing

--- src/audiomodule/speechToText/speechToText.py
+++ src/audiomodule/speechToText/speechToText.py
@@ -1,3 +1,5 @@
+import json
+
 from vosk import Model, KaldiRecognizer
 import pyaudio
 from pathlib import Path
@@ -9,7 +11,20 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
-while True:
-    data = stream.read(4096)
-    if recognizer.AcceptWaveform(data):
-        print(recognizer.Result())
+
+def start_audio_detection():
+
+    print("Start speaking...")
+
+    with open("audioToText.txt", "a") as f:
+        while True:
+            data = stream.read(8192)
+
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                result_json = json.loads(result)
+                text = result_json.get('text', '')
+
+                if text:
+                    print(f"Recognized: {text}")
+                    f.write(text + "\n")
