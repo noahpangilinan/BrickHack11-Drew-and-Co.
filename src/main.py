8241a45764405a7c47d2d2f6aa20abf068f44fca
Noah Pangilinan <noahpangilinan1@gmail.com>
2025-02-22 18:06:37 -0500

    Test speech and function calls from main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.audiomodule.speechToText.speechToText import start_audio_detection
+from src.audiomodule.speechToText.speechScript import readSpeech
 print("my glorious king drew")
-
+readSpeech()
 start_audio_detection()
